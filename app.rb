9b30c8530f1d25688254810b170859f8de995bc5
theartist <kasnar2020@gmail.com>
2015-10-30 12:03:26 -0700

    Update app.rb

--- app.rb
+++ app.rb
@@ -2,9 +2,9 @@
 
 require 'yaml'
 require 'rest-client'
-require 'v1member'
+require './lib/v1member'
 
-if File.file?('v1config_IPW.yml')
+if File.file?('./config/v1config_IPW.yml')
   v1 = V1Connect.new()
   get_members=v1.get_memberlist
 end
