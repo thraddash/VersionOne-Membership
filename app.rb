541e553a1e1155b1e78ade1f25d7d3f23cf38f8d
theartist <kasnar2020@gmail.com>
2015-11-01 16:07:00 -0800

    Update app.rb
    rename v1config_IPW.yml to v1config.yml

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ require 'yaml'
 require 'rest-client'
 require './lib/v1member'
 
-if File.file?('./config/v1config_IPW.yml')
+if File.file?('./config/v1config.yml')
   v1 = V1Connect.new()
   get_members=v1.get_memberlist
 end
